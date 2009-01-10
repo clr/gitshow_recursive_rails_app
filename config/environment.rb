a8c8cb294236d45dfdccdcb7c2e1fb4746c3d8c0
CLR <clr@localhost.localdomain>
2009-01-10 12:30:58 -0500

    -- froze Rails 2.2.2 into vendor --

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,9 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-  config.gem 'rails', :version => '2.2.2'
+#  config.gem 'rails', :version => '2.2.2'
   config.gem 'mojombo-grit', :source => 'http://gems.github.com', :lib => 'grit'
-  config.gem 'haml', :version => '2.0.4'
+#  config.gem 'haml', :version => '2.0.4'
 
   config.plugins = [ :all ]
 
